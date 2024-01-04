697b6ad13b20653c01e728d662e13a7064944645
alexanderhodes <alexander.hodes@live.com>
2024-01-04 20:20:03 +0100

    feat: added dark mode

---
+++ src/components/ThemeToggle.tsx
@@ -0,0 +1,26 @@
+import { type FunctionComponent, useEffect, useState } from "react";
+
+interface ThemeToggleProps {
+  children: React.ReactNode;
+}
+
+const ThemeToggle: FunctionComponent<ThemeToggleProps> = ({ children }) => {
+  const [theme, setTheme] = useState(localStorage.getItem("theme") ?? "light");
+
+  const handleClick = () => {
+    setTheme(theme === "light" ? "dark" : "light");
+  };
+
+  useEffect(() => {
+    if (theme === "dark") {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }, [theme]);
+
+  return <button onClick={handleClick}>{children}</button>;
+};
+
+export default ThemeToggle;
