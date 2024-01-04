d307d9575abcf92f4f16ffce6e6c6abd553f5c8e
alexanderhodes <alexander.hodes@live.com>
2024-01-04 15:21:41 +0100

    chore: updated dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
+import icon from "astro-icon";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), icon()],
   vite: {
     ssr: {
       external: ["svgo"],
---
+++ ideas.md
@@ -0,0 +1,18 @@
+# Ideen
+
+## Inhalte
+
+- Projekte
+- Arbeit
+- Technologien (typescript, react, react native, angular, nest.js, astro)
+- Blog
+- Links -> Social Media
+
+## Sonstiges
+
+- E-Mail Adresse mit eigener Domain: https://tutanota.com/de/ (1€ pro Jahr) -> für Premium
+- Metrical Integration (50$ pro Jahr)
+
+## Inspiration
+
+- https://linkedup.littlesticks.dev (Link Bio Page)
