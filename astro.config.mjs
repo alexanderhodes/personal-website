cee4aa4f90e270fd6ba0110b5009b81c3e5510f4
alexanderhodes <alexander.hodes@live.com>
2024-01-05 14:22:43 +0100

    feat: added sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,10 +6,33 @@ import react from "@astrojs/react";
 import markdoc from "@astrojs/markdoc";
 import keystatic from "@keystatic/astro";
 import vercel from "@astrojs/vercel/serverless";
+import robotsTxt from "astro-robots-txt";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx(), icon(), react(), markdoc(), keystatic()],
+  site: "https://alexanderhodes.de",
+  integrations: [
+    tailwind(),
+    mdx(),
+    icon(),
+    react(),
+    markdoc(),
+    keystatic(),
+    robotsTxt({
+      policy: [
+        {
+          userAgent: "*",
+          disallow: ["/keystatic/*", "/api/keystatic/*"],
+        },
+      ],
+    }),
+    sitemap({
+      filter: (page) =>
+        page !== "https://stargazers.club/keystatic/" &&
+        page !== "https://stargazers.club/api/keystatic/",
+    }),
+  ],
   output: "hybrid",
   vite: {
     ssr: {
